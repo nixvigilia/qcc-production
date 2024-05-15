dcb2eefcf2e96b35e5fd466f908d1989dd53741c
Nixoy <john.nicholai.vigilia@gmail.com>
2024-05-15 22:51:32 +0800

    wip

---
+++ app/layout.js
@@ -0,0 +1,28 @@
+import Providers from "@/components/layout/providers";
+import { Toaster } from "@/components/ui/toaster";
+import "@uploadthing/react/styles.css";
+
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { getServerSession } from "next-auth";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Next Shadcn",
+  description: "Basic dashboard with Next.js and Shadcn",
+};
+
+export default async function RootLayout({ children }) {
+  const session = await getServerSession();
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} overflow-hidden`}>
+        <Providers session={session}>
+          <Toaster />
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
